532b6907ac1301f80c1b54e45b18e8ccd9eb330d
manu-devops98 <manuelcappello7@gmail.com>
2021-11-24 16:11:15 +0100

    add esercizio + commenti

--- mail/js/script.js
+++ mail/js/script.js
@@ -10,19 +10,25 @@ const mail = ['manuel@gmail', 'cappello@yahoo.it', 'manuelcappello7@gmail.com','
 
 // seleziono container html 
 const container = document.querySelector('.container');
-
+// creo h1 
 let element = document.createElement('h1');
+// creo variabile find per il login 
+let login = false;
 
 for (let i = 0; i < mail.length; i++) {
     const arrayElements = mail[i];
     console.log(arrayElements);
-
-    if (userEmail != arrayElements) {
-        element.innerHTML = 'Accesso negato! Riprova';
-    } else {
-        element.innerHTML += 'Benvenuto ' + userEmail;
-    }
-    
+// istruzione if per determinare se useremail = elemento dell'array 
+    if (userEmail.toLowerCase() == arrayElements.toLowerCase()) {
+    // la variabile login diventa true solo nel caso in cui useremail è uguale a uno degli elementi dell'array     
+        login = true;
+    } 
+}
+// istruzione if per stampare messaggio in pagina 
+if (login) {
+    element.innerHTML += 'Benvenuto ' + userEmail;
+} else {
+    element.innerHTML += 'Accesso negato! Riprova';
 }
 
 container.append(element);
