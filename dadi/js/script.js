433fcdbcc6c18526e41baf1c832de34a909f0666
manu-devops98 <manuelcappello7@gmail.com>
2021-11-24 16:40:46 +0100

    end

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -4,24 +4,25 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 // genero numeri casuali da 1 a 6 per giocatore e computer
 const numberPlayer = Math.floor(Math.random() * 6) + 1;
 const numberComputer = Math.floor(Math.random() * 6) + 1;
-console.log(numberPlayer);
-console.log(numberComputer);
 
+// seleziono elementi in html 
 const player = document.getElementById('player');
 const computer = document.getElementById('computer');
 const container = document.querySelector('.container');
 
-
+// appendo numeri random di giocatore e computer in html 
 player.append(numberPlayer);
 computer.append(numberComputer);
 
+// creo h1 html 
 const element = document.createElement('h1');
-element.innerHTML;
+
+// istruzione if per determinare se num-giocatore o num-computer è più alto 
 if (numberPlayer > numberComputer) {
     element.innerHTML += 'Giocatore vince avendo numero ' + numberPlayer + ' ' + 'e Computer numero ' + numberComputer;
 } else {
     element.innerHTML += 'Computer vince avendo numero ' + numberComputer + ' ' + 'e Giocatore numero ' + numberPlayer;
 }
-
+// appendo h1 in html 
 container.append(element)
 
